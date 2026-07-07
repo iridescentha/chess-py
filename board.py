3826dde1f4d238255888a1b2b8af1df94c7c6aa5
Iridescent <hugo.afiza.adinanta@gmail.com>
2026-07-07 18:19:43 +0700

    first commit

---
+++ board.py
@@ -0,0 +1,92 @@
+import pygame
+
+piece_width = 80
+piece_height = 80
+white_pawn = pygame.transform.scale(pygame.image.load("assets/white_pawn.png"), (piece_width, piece_height))
+white_rook = pygame.transform.scale(pygame.image.load("assets/white_rook.png"), (piece_width, piece_height))
+white_knight = pygame.transform.scale(pygame.image.load("assets/white_knight.png"), (piece_width, piece_height))
+white_bishop = pygame.transform.scale(pygame.image.load("assets/white_bishop.png"), (piece_width, piece_height))
+white_queen = pygame.transform.scale(pygame.image.load("assets/white_queen.png"), (piece_width, piece_height))
+white_king = pygame.transform.scale(pygame.image.load("assets/white_king.png"), (piece_width, piece_height))
+
+black_pawn = pygame.transform.scale(pygame.image.load("assets/black_pawn.png"), (piece_width, piece_height))
+black_rook = pygame.transform.scale(pygame.image.load("assets/black_rook.png"), (piece_width, piece_height))
+black_knight = pygame.transform.scale(pygame.image.load("assets/black_knight.png"), (piece_width, piece_height))
+black_bishop = pygame.transform.scale(pygame.image.load("assets/black_bishop.png"), (piece_width, piece_height))
+black_queen = pygame.transform.scale(pygame.image.load("assets/black_queen.png"), (piece_width, piece_height))
+black_king = pygame.transform.scale(pygame.image.load("assets/black_king.png"), (piece_width, piece_height))
+
+class Board:
+    boardState = [
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    ["",  "",  "",  "",  "",  "",  "",  ""],
+    ["",  "",  "",  "",  "",  "",  "",  ""],
+    ["",  "",  "",  "",  "",  "",  "",  ""],
+    ["",  "",  "",  "",  "",  "",  "",  ""],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ]
+
+    square_size = 80
+    board_left = 320
+    board_top = 40
+    board_size = 640
+
+    piece_image = {
+        "P": white_pawn,
+        "R": white_rook,
+        "N": white_knight,
+        "B": white_bishop,
+        "Q": white_queen,
+        "K": white_king,
+
+        "p": black_pawn,
+        "r": black_rook,
+        "n": black_knight,
+        "b": black_bishop,
+        "q": black_queen,
+        "k": black_king,
+    }
+
+    def __init__(self):
+        pass
+
+    def drawBoard(self, screen, screen_rect):
+        rect = pygame.Rect(0, 0, self.board_size, self.board_size)
+        rect.center = screen_rect.center
+        pygame.draw.rect(screen, "black", rect, 5)
+
+    def drawSquares(self, screen,):
+        for row in range(8):
+            y_pos = self.board_top + (row * self.square_size)
+            for column in range(8):
+                    if row % 2 and column % 2:
+                        x_pos2 = self.board_left + (column * self.square_size)
+                        pygame.draw.rect(screen, "burlywood1", pygame.Rect(x_pos2, y_pos,self.square_size, self.square_size))
+                    elif row % 2 or column % 2:
+                        x_pos = self.board_left + (column * self.square_size)
+                        pygame.draw.rect(screen, "brown4", pygame.Rect(x_pos, y_pos, self.square_size, self.square_size))
+                    else:
+                        x_pos2 = self.board_left + (column * self.square_size)
+                        pygame.draw.rect(screen, "burlywood1", pygame.Rect(x_pos2, y_pos, self.square_size, self.square_size))
+
+    def drawPieceCentered(self, image, row, col, screen):
+        center_square_x = self.board_left + (row * self.square_size + (self.square_size // 2))
+        center_square_y = self.board_top + (col * self.square_size + (self.square_size // 2))
+        rect = image.get_rect()
+        rect.center = (center_square_x, center_square_y)
+        screen.blit(image, rect)
+        
+
+    def drawPieces(self, screen):
+        for row in range(8):
+            for col in range(8):
+                piece = self.boardState[col][row]
+                if piece != "":
+                    image_piece = self.piece_image[piece]
+                    self.drawPieceCentered(image_piece, row, col, screen)
+
+    def movePiece(self, current_row, current_col, end_row, end_col, piece, boardState):
+        boardState[current_row][current_col] = ""
+        boardState[end_row][end_col] = piece
---
+++ main.py
@@ -0,0 +1,70 @@
+import pygame
+from board import Board
+from sys import exit
+
+pygame.init()
+pygame.display.set_caption("Chess")
+screen = pygame.display.set_mode((1280, 720))
+screen_rect = screen.get_rect()
+clock = pygame.time.Clock()
+
+def getClickedSquare():
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    local_x, local_y = mouse_x - 320, mouse_y - 40
+    if local_x >= 0 and local_y >= 0 and local_x < 640 and local_y < 640:
+        square_x = local_x // 80
+        square_y = local_y // 80
+        return [square_y, square_x]
+    return None
+
+def main():
+    running = True
+    board = Board()
+    selected_square = None
+    while running:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                clicked_position = getClickedSquare()
+                if not clicked_position: continue
+                
+                if selected_square is None:
+                        if clicked_position != None:
+                            clicked_row, clicked_col = clicked_position[0], clicked_position[1]
+                        if board.boardState[clicked_row][clicked_col] != "":
+                            selected_square = [clicked_row, clicked_col]
+                        
+                else:
+                    start_row, start_col = selected_square[0], selected_square[1]
+                    clicked_position = getClickedSquare()
+
+                    if clicked_position != None: 
+                        end_row, end_col = clicked_position[0], clicked_position[1]
+                    else: 
+                        continue
+
+                    piece = board.boardState[start_row][start_col]
+                    board.movePiece(start_row, start_col, end_row, end_col, piece, board.boardState)
+                    selected_square = None
+                
+                
+        screen.fill("white")
+        
+        board.drawSquares(screen)
+        board.drawBoard(screen, screen_rect)
+        board.drawPieces(screen)
+
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
+    exit()
+
+
+if __name__ == "__main__":
+    main()
